40667d29bcf40015735afabe5e55dba1b4a94cb2
vanshu778 <vthesiya778@gmail.com>
2025-01-16 20:47:41 +0530

    Ignore files

--- patterns/tempCodeRunnerFile.cpp
+++
@@ -1,9 +0,0 @@
-  for(int i = 5; i >= 1; i--) {
-      for(int j = 5; j >= 1; j--) {
-          if(j == i)
-              cout << "*";
-          else
-              cout << j;
-      }
-      cout << endl;
-  }
